66c72ba49d5e0701d63d5bbed2738e905378d63e
Nicolas Bazille <483842+oltodo@users.noreply.github.com>
2022-06-22 10:57:01 +0200

    feat: implement prod GraphQL endpoint

--- src/services/appolo.ts
+++ src/services/appolo.ts
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "http://localhost:8000/graphql",
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8000/graphql"
+      : "http://wp.egliselyongerland.org/graphql",
   cache: new InMemoryCache(),
 });
 
