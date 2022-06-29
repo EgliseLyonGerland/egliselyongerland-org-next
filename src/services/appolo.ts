c0874052867d52f2fd3a6a2451cb606d51f7aa2e
Nicolas Bazille <483842+oltodo@users.noreply.github.com>
2022-06-29 17:10:56 +0200

    chore: use https for Wordpress endpoint

--- src/services/appolo.ts
+++ src/services/appolo.ts
@@ -4,7 +4,7 @@ export const client = new ApolloClient({
   uri:
     process.env.NODE_ENV === "development"
       ? "http://localhost:8000/graphql"
-      : "http://wp.egliselyongerland.org/graphql",
+      : "https://wp.egliselyongerland.org/graphql",
   cache: new InMemoryCache(),
 });
 
